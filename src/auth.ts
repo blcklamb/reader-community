f3e939a973c7e187d3b093e37ff9b3d7a4b33d31
blcklamb <whenucan35@gmail.com>
2024-03-29 16:40:23 +0900

    Feat: signin callback in google signin

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,9 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-("next-auth/providers/google");
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const authOptions: NextAuthConfig = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -15,4 +11,48 @@ export const {
     }),
   ],
   secret: process.env.AUTH_SECRET,
+};
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  ...authOptions,
+  callbacks: {
+    async signIn({ profile }) {
+      if (profile) {
+        const prisma = new PrismaClient();
+        try {
+          const user = await prisma.user.findUnique({
+            where: {
+              email: profile.email || "",
+            },
+          });
+          if (!user) {
+            if (profile.at_hash && profile.email && profile.name) {
+              await prisma.user.create({
+                data: {
+                  id: profile.at_hash + "",
+                  email: profile.email,
+                  name: profile.name,
+                  image:
+                    profile.picture + "" ||
+                    `https://ui-avatars.com/api/?name=${profile.name}`,
+                },
+              });
+              return true;
+            } else {
+              return false;
+            }
+          }
+          return true;
+        } catch (e) {
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
